1dd98dd194942d70054e7cfd07038317a7c214f2
Volley <75976164@qq.com>
2016-09-10 21:43:12 +0800

    init

---
+++ src/main/java/cn/xmt/spark/SparkMain.kt
@@ -0,0 +1,18 @@
+package cn.xmt.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import scala.Tuple2
+
+fun main(args: Array<String>) {
+    val inputFile = args[0]
+    val outputFile = args[1]
+
+    val conf = SparkConf().setAppName("wordCount")
+    val sc = JavaSparkContext(conf)
+
+    val input = sc.textFile(inputFile)
+    val words = input.flatMap { it.split(" ") as MutableIterator<*>? }
+    val counts = words.mapToPair { Tuple2(it, 1) }.reduceByKey { x, y -> x + y }
+    counts.saveAsTextFile(outputFile)
+}
